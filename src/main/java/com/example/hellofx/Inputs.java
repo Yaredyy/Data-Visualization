bccc963c8b0472a258e3f673a95aa1426d0e2096
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-01-20 18:23:24 -0500

    base file name change
    from data.java to inputs.java

--- src/main/java/com/example/hellofx/Inputs.java
+++ src/main/java/com/example/hellofx/Inputs.java
@@ -1,95 +1,128 @@
-//package
+//required package currently, might get removed if alternative found.
 package com.example.hellofx;
 
-//imports
+//import class
+import javafx.application.Application;
+import javafx.geometry.Pos;
+// import javafx.event.Event;
+// import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
 import java.util.ArrayList;
 
-//class
-public class Inputs {
+//import of exception
+import java.io.IOException;
 
-    //root variable
-    Node root;
+//class signature
+public class Inputs extends Application {
+    //main page
+    @Override
+    public void start(Stage stage) throws IOException {
+        int x = 1;
+        int y = 1;
+        ArrayList<TextField> inputs = new ArrayList<TextField>();
 
-    //list constructor
-    public Inputs() {
-    }
+        //header
+        Text head = new Text("Inputs");
+        head.setFont(Font.font("Arial", FontWeight.BOLD, 36));
 
-    //adding a new field
-    public void add(int x, int y){
+        //header HBox
+        HBox header = new HBox(head);
+        header.setAlignment(Pos.BASELINE_CENTER);
 
-        Node newNode = new Node(x,y);
+        
+        //button for adding
+        Button addField = new Button();
+        
+        
+        addField.setText("Add input field");
+        addField.setOnAction(actionEvent ->  {
+            //change
+        });
+        
+        //button for removing a field
+        Button removeField = new Button();
+        
+        removeField.setText("Remove input field");
+        removeField.setOnAction(actionEvent ->  {
+            //change
+        });
 
-         if (root==null){
-            root = newNode;
-         }
-         else if(root.next==null){
-            root.next=newNode;
-         }
-         else{
-             Node temp = root;
-             while (temp.next!=null){
-                 temp=temp.next;
-             }
-             temp.next = newNode;
-         }
-    }
+        //button for computing a field
+        Button compute = new Button();
+        
+        compute.setText("Graph input fields");
+        compute.setOnAction(actionEvent ->  {
+            //change
+        });
+        
+        
+        //vbox for options
+        HBox options = new HBox(addField, removeField, compute);
+        options.setAlignment(Pos.BASELINE_CENTER);
 
-    //removing last field
-    public void remove(){
+        //main screen
+        BorderPane screen = new BorderPane();
 
-        if(root==null){}
-        else{
-            Node temp=root;
-
-            while(temp.next.next!=null){
-                temp=temp.next;
-            }
-            
-            temp.next = null;
-
-        }
-    }
-
-
-    //toString to recieve value of each input. most likely not to be used and will be deleted.
-    public String toString() {
-
-        Node temp = root;
-        String result = "";
-
-        while (temp != null) {
-
-            result = result + "(" + temp.x + "," + temp.y + ")";
-            temp = temp.next;
-
-        }
-
-        return result;
-
-    }
+        //border components
+        screen.setTop(header);
+        // screen.setCenter();
+        screen.setBottom(options);
 
 
-
-
-
-    //Node class
-    public class Node {
-
-        //Node variables
-        Integer y;
-        Integer x;
-        Node next;
-
-        //Node constructor
-        public Node(int x, int y) {
-
-            this.x = x;
-            this.y = y;
-
-        }
-
+        //outputs to screen
+        Scene scene = new Scene(screen, 495,810);
+        stage.setTitle("Inputs!");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
 
+    //compute graph
+    public void grapher(){
+
+        //header
+        Text graphText = new Text("Graph");
+        graphText.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+        
+        //header hbox
+        HBox graphHeader = new HBox(graphText);
+        graphHeader.setAlignment(Pos.BASELINE_CENTER);
+        
+        //y values
+
+
+        
+        
+        
+        
+        
+        
+        
+        //borderpane
+        BorderPane graph = new BorderPane();
+        graph.setTop(graphHeader);
+
+        //uses method to show graph
+        Scene scene = new Scene(graph,495,810);
+        grapherStage(scene);
+    }
+
+    //show graph
+    public void grapherStage(Scene scene){
+        //creates window of inputed scene
+        Stage stage2 = new Stage();
+        stage2.setTitle("Graph");
+        stage2.setScene(scene);
+        stage2.show();
+    }
+
+    //runner
+    public static void main(String[] args) {
+        launch();
+    }
 }
