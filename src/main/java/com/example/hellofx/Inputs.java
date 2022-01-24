9b4fea8f7922dad0285a5ffaa193af59c25a1954
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-01-24 00:46:01 -0500

    more stuff
    fixed grapher scene and added inputs shit
    
    need to figure out error issue

--- src/main/java/com/example/hellofx/Inputs.java
+++ src/main/java/com/example/hellofx/Inputs.java
@@ -9,9 +9,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
-import java.util.ArrayList;
+// import java.util.ArrayList;l
 
 //import of exception
 import java.io.IOException;
@@ -21,9 +22,6 @@ public class Inputs extends Application {
     //main page
     @Override
     public void start(Stage stage) throws IOException {
-        int x = 1;
-        int y = 1;
-        ArrayList<TextField> inputs = new ArrayList<TextField>();
 
         //header
         Text head = new Text("Inputs");
@@ -33,35 +31,71 @@ public class Inputs extends Application {
         HBox header = new HBox(head);
         header.setAlignment(Pos.BASELINE_CENTER);
 
-        
+        //input fields
+
+        //field 1
+        Text text1 = new Text("Input field 1: ");
+        TextField field1 = new TextField();
+
+        HBox textField1 = new HBox(text1, field1);
+
+        //field 2
+        Text text2 = new Text("Input field 2: ");
+        TextField field2 = new TextField();
+
+        HBox textField2 = new HBox(text2, field2);
+
+        //field 3
+        Text text3 = new Text("Input field 3: ");
+        TextField field3 = new TextField();
+
+        HBox textField3 = new HBox(text3, field3);
+
+        //field 4
+        Text text4 = new Text("Input field 4: ");
+        TextField field4 = new TextField();
+
+        HBox textField4 = new HBox(text4, field4);
+
+        //field 5
+        Text text5 = new Text("Input field 5: ");
+        TextField field5 = new TextField();
+
+        HBox textField5 = new HBox(text5, field5);
+
+
+        VBox fields = new VBox(textField1, textField2, textField3, textField4, textField5);
+
+        fields.setAlignment(Pos.BASELINE_CENTER);
+
         //button for adding
-        Button addField = new Button();
+        // Button addField = new Button();
         
         
-        addField.setText("Add input field");
-        addField.setOnAction(actionEvent ->  {
-            //change
-        });
+        // addField.setText("Add input field");
+        // addField.setOnAction(actionEvent ->  {
+        //     //change
+        // });
         
         //button for removing a field
-        Button removeField = new Button();
+        // Button removeField = new Button();
         
-        removeField.setText("Remove input field");
-        removeField.setOnAction(actionEvent ->  {
-            //change
-        });
+        // removeField.setText("Remove input field");
+        // removeField.setOnAction(actionEvent ->  {
+        //     //change
+        // });
 
         //button for computing a field
         Button compute = new Button();
         
         compute.setText("Graph input fields");
         compute.setOnAction(actionEvent ->  {
-            //change
+            grapher(textField1, textField2, textField3, textField4, textField5);
         });
         
         
         //vbox for options
-        HBox options = new HBox(addField, removeField, compute);
+        HBox options = new HBox(compute);
         options.setAlignment(Pos.BASELINE_CENTER);
 
         //main screen
@@ -69,7 +103,7 @@ public class Inputs extends Application {
 
         //border components
         screen.setTop(header);
-        // screen.setCenter();
+        screen.setCenter(fields);
         screen.setBottom(options);
 
 
@@ -83,7 +117,7 @@ public class Inputs extends Application {
 
 
     //compute graph
-    public void grapher(){
+    public void grapher(Object textField1, Object textField2, Object textField3, Object textField4, Object textField5){
 
         //header
         Text graphText = new Text("Graph");
@@ -93,19 +127,39 @@ public class Inputs extends Application {
         HBox graphHeader = new HBox(graphText);
         graphHeader.setAlignment(Pos.BASELINE_CENTER);
         
-        //y values
+        //graph
+
+        //rectangle 1
+        Rectangle chart1 = new Rectangle();
+        chart1.setWidth(10);
+        chart1.setHeight(Integer.parseInt(((Text) textField1).getText()));
+
+        //rectangle 2
+        Rectangle chart2 = new Rectangle();
+        chart2.setWidth(10);
+        chart2.setHeight(Integer.parseInt(((Text) textField2).getText()));
+
+        //rectangle 3
+        Rectangle chart3 = new Rectangle();
+        chart3.setWidth(10);
+        chart3.setHeight(Integer.parseInt(((Text) textField3).getText()));
+
+        //rectangle 4
+        Rectangle chart4 = new Rectangle();
+        chart4.setWidth(10);
+        chart4.setHeight(Integer.parseInt(((Text) textField4).getText()));
+
+        //rectangle 5
+        Rectangle chart5 = new Rectangle();
+        chart5.setWidth(10);
+        chart5.setHeight(Integer.parseInt(((Text) textField5).getText()));
 
 
-        
-        
-        
-        
-        
-        
-        
+        VBox graphBox = new VBox(chart1, chart2, chart3, chart4, chart5);
         //borderpane
         BorderPane graph = new BorderPane();
         graph.setTop(graphHeader);
+        graph.setCenter(graphBox);
 
         //uses method to show graph
         Scene scene = new Scene(graph,495,810);
